76d911ec47526259059a3ab9fb78f07e65b21e4c
Tommy Van Der Vaart <tommyvdv@hotmail.nl>
2018-08-01 15:36:41 +0200

    afgewerkt

--- Testpage.js
+++ Testpage.js
@@ -34,41 +34,44 @@ function RandomGradient() {
 	h3.textContent = body.style.background + ";";
 }
 
-// let numbers = [1,5,98,56,78,109,1,3,6,8,7,3,54,7,9,2,9,33,42];
+let lijst1 = [1,5,98,56,78,109,1,3,6,8,7,3,54,7,9,2,9,33,42];
+let lijst2 = [0, 1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,5,7,9,56,1,2,3,6,7,8,34,55,77,87,12,43,76,3,3,3,3,56,99];
 
-// numbers.sort((a,b) => a-b);
+numbers.sort((a,b) => a-b);
 
-// function sortlist() {
+function sortlist(numbers) {
 
-// 	let array2 = [];
-//      let array1 = [numbers[0]];
+	let array2 = [numbers[0]];
+    let array1 = [numbers[0]];
 
-// for (var i =1; i< numbers.length; i++)
-// {
+for (var i =1; i< numbers.length; i++)
+{
 	
-// 	if (numbers[i-1]===numbers[i])
-// 	{
-// 		array1.push(numbers[i]);
-// 		console.log(array1);
-// 	}
-//      else if (i===1)
-//      {
-//             array1 = [];
-//              array1.push(numbers[i]);
-// 		array2.push(numbers[i]);
-// 		console.log(array2);
-//      }
-// 	else 
-// 	{
-//              array2.push(array1);
-//              array1 = [];
-//              array1.push(numbers[i]);
-// 		array2.push(numbers[i]);
-// 		console.log(array2);
-// 	}
+	if (numbers[i-1]===numbers[i])
+	{
+		array1.push(numbers[i]);
+		console.log(array1);
+	}
+	else 
+	{
+		if(array1.length === 1)
+		{
+			array1.pop();
+            array1.push(numbers[i]);
+			array2.push(numbers[i]);
+		}
+		else
+		{
+			array2.pop();
+			array2.push(array1);
+            array1 = [];
+			array1.push(numbers[i]);
+			array2.push(numbers[i]);
+		}
+	}
 
 
-// }
-// return array2;
+}
+return array2;
 
 }
